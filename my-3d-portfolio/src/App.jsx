0969ee59d224b3a8a24da2ebf3317e65931113cd
Empire688682 <asehindej@gmail.com>
2024-09-14 03:54:44 +0100

    first commit

---
+++ my-3d-portfolio/src/App.jsx
@@ -0,0 +1,46 @@
+import React, { useRef } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { MeshStandardMaterial, BoxGeometry } from 'three';
+import { useFrame } from '@react-three/fiber';
+import './App.css'
+
+const SpinningBox = () => {
+  const meshRef = useRef();
+
+  // Rotate the box every frame
+  useFrame(() => {
+    if (meshRef.current) {
+      meshRef.current.rotation.x += 0.01;
+      meshRef.current.rotation.y += 0.01;
+    }
+  });
+
+  return (
+    <mesh ref={meshRef}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={'#ffff'} />
+    </mesh>
+  );
+};
+
+
+
+const Scene = () => {
+  return (
+    <Canvas>
+      <ambientLight intensity={1} />
+      <pointLight position={[10, 10, 10]} />
+      <SpinningBox />
+    </Canvas>
+  );
+};
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Scene/>
+    </div>
+  )
+}
+
+export default App
