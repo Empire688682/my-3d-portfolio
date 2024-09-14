ae8562348d878caa44638de103d147d681984d9b
Empire688682 <asehindej@gmail.com>
2024-09-14 11:51:01 +0100

    all

--- my-3d-portfolio/src/Component/Footer/Footer.jsx
+++ my-3d-portfolio/src/Component/Footer/Footer.jsx
@@ -1,30 +1,21 @@
 import React from 'react';
 import './Footer.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTwitter, faGithub, faLinkedin, faFacebook, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { socials } from '../Constant';
 
 const Footer = () => {
   return (
     <div className='footer'>
         <div className="link_icons">
-        <a href="https://twitter.com">
-        <FontAwesomeIcon icon={faTwitter} className='icon' size="2x" />
-      </a>
-      <a href="https://github.com">
-        <FontAwesomeIcon icon={faGithub} className='icon' size="2x" />
-      </a>
-      <a href="https://github.com">
-        <FontAwesomeIcon icon={faLinkedin} className='icon' size="2x" />
-      </a>
-      <a href="https://github.com">
-        <FontAwesomeIcon icon={faInstagram} className='icon' size="2x" />
-      </a>
-      <a href="https://github.com">
-        <FontAwesomeIcon icon={faFacebook} className='icon' size="2x" />
-      </a>
-      <a href="https://github.com">
-        <FontAwesomeIcon icon={faYoutube} className='icon' size="2x" />
-      </a>
+        {
+            socials.map((social) => {
+                return (
+                    <a key={social.id} href={social.url} className="icon" target="_blank" rel="noreferrer">
+                        <FontAwesomeIcon icon={social.icon} size='2x' />
+                    </a>
+                )
+            })
+        }
         </div>
       <p>© 2023 Jayempire.</p>
     </div>
