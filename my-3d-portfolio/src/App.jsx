6530fd20c1ebd0d25541a0a0cbde0370df09688d
Empire688682 <asehindej@gmail.com>
2024-09-19 00:26:14 +0100

    new track

--- my-3d-portfolio/src/App.jsx
+++ my-3d-portfolio/src/App.jsx
@@ -1,15 +1,7 @@
-import React, { useRef } from 'react';
-import './App.css'
-import Navbar from './Component/Navbar/Navbar';
-import Footer from './Component/Footer/Footer';
-import Hero from './Component/Hero/Hero';
-
 const App = () => {
   return (
     <div className='app'>
-     <Navbar/>
-      <Hero/>
-      <Footer/>
+     3D Developer Portfolio
     </div>
   )
 }
--- my-3d-portfolio/src/main.jsx
+++ my-3d-portfolio/src/main.jsx
@@ -2,15 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom';
-import { AppProvider } from './Component/Context.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <AppProvider>
-        <App />
-      </AppProvider>
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 )
