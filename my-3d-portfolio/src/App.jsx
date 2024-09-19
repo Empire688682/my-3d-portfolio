70be65c74ae0c831831fb7639b9955a0aeb5141b
Empire688682 <asehindej@gmail.com>
2024-09-19 01:19:44 +0100

    error resolved

--- my-3d-portfolio/src/App.jsx
+++ my-3d-portfolio/src/App.jsx
@@ -1,8 +1,25 @@
+import { BrowserRouter } from 'react-router-dom';
+import { About, Contact, Hero, Navbar, Tech, Works, Loader, Feedbacks, StarsCanvas, Experience } from './components';
 const App = () => {
   return (
-    <div className='app'>
-     3D Developer Portfolio
-    </div>
+    <BrowserRouter>
+      <div className='relative z-0 bg-primary'>
+        <div className="big-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Tech />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
