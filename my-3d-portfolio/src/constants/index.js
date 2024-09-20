77f79391e24eaa629ad7821563812b9df00323ad
Empire688682 <asehindej@gmail.com>
2024-09-20 14:18:07 +0100

    portfolio

--- my-3d-portfolio/src/constants/index.js
+++ my-3d-portfolio/src/constants/index.js
@@ -101,8 +101,6 @@ const technologies = [
 const experiences = [
   {
     title: "React.js Developer",
-    company_name: "Starbucks",
-    icon: starbucks,
     iconBg: "#383E56",
     date: "March 2020 - April 2021",
     points: [
@@ -114,28 +112,24 @@ const experiences = [
   },
   {
     title: "Web Developer",
-    company_name: "Shopify",
-    icon: shopify,
     iconBg: "#383E56",
     date: "Jan 2022 - Jan 2023",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      "Developing and maintaining dynamic, user-centric websites using modern web technologies such as HTML5, CSS3, and JavaScript, Tailwind, and React.",
+      "Collaborating with designers and content creators to build engaging, responsive, and accessible websites.",
+      "Optimizing website performance and ensuring cross-browser compatibility for a seamless user experience.",
+      "Utilizing version control systems (Git) and participating in collaborative workflows with feedback-driven code reviews.",
     ],
   },
   {
     title: "Full stack Developer",
-    company_name: "Meta",
-    icon: meta,
-    iconBg: "#E6DEDD",
+    iconBg: "#383E56",
     date: "Jan 2023 - Present",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      "Designing, developing, and maintaining scalable web applications using both front-end (React.js) and back-end (Node.js, Express, MongoDB) technologies.",
+      "Collaborating with cross-functional teams to deliver comprehensive full-stack solutions, from UI/UX to server-side logic and databases.",
+      "Implementing secure, efficient APIs and ensuring proper communication between front-end and back-end systems.",
+      "Conducting code reviews and offering constructive feedback, ensuring high standards for code quality and performance across all layers of the stack.",
     ],
   },
 ];
@@ -147,7 +141,7 @@ const testimonials = [
     name: "Sara Lee",
     designation: "CFO",
     company: "Acme Co",
-    image: "https://randomuser.me/api/portraits/women/4.jpg",
+    image: "https://randomuser.me/api/portraits/women/9.jpg",
   },
   {
     testimonial:
@@ -155,7 +149,7 @@ const testimonials = [
     name: "Chris Brown",
     designation: "COO",
     company: "DEF Corp",
-    image: "https://randomuser.me/api/portraits/men/5.jpg",
+    image: "https://randomuser.me/api/portraits/men/70.jpg",
   },
   {
     testimonial:
@@ -163,7 +157,7 @@ const testimonials = [
     name: "Lisa Wang",
     designation: "CTO",
     company: "456 Enterprises",
-    image: "https://randomuser.me/api/portraits/women/6.jpg",
+    image: "https://randomuser.me/api/portraits/women/69.jpg",
   },
 ];
 
@@ -183,6 +177,10 @@ const projects = [
       },
       {
         name: "express.js",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "node.js",
         color: "pink-text-gradient",
       },
       {
@@ -194,20 +192,28 @@ const projects = [
     source_code_link: "https://github.com/",
   },
   {
-    name: "Job IT",
+    name: "Divas Collective",
     description:
       "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
     tags: [
       {
-        name: "react",
+        name: "nextjs",
         color: "blue-text-gradient",
       },
       {
-        name: "restapi",
+        name: "mongodb",
         color: "green-text-gradient",
       },
       {
-        name: "scss",
+        name: "css",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "javascript",
+        color: "green-text-gradient",
+      },
+      {
+        name: "paystack",
         color: "pink-text-gradient",
       },
     ],
@@ -215,21 +221,42 @@ const projects = [
     source_code_link: "https://github.com/",
   },
   {
-    name: "Trip Guide",
+    name: "Idea Jotter",
     description:
       "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
     tags: [
       {
-        name: "nextjs",
+        name: "react",
         color: "blue-text-gradient",
       },
       {
-        name: "supabase",
+        name: "mongodb",
         color: "green-text-gradient",
       },
       {
-        name: "css",
+        name: "node.js",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "express.js",
         color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Home-Fittings",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "css",
+        color: "green-text-gradient",
       },
     ],
     image: tripguide,
