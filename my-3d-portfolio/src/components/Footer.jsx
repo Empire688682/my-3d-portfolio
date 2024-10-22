2118b8d12e34dd58ed4ad2ad7c00810d27ea570a
Empire688682 <asehindej@gmail.com>
2024-10-22 17:24:21 +0100

    footer

--- my-3d-portfolio/src/components/Footer.jsx
+++ my-3d-portfolio/src/components/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
         I collaborate closely with clients to create efficient, scalable, and user-friendly solutions that solve real-world problems. Let's work together to bring your ideas to life.
         </p>
       </div>
-      <div className="flex-1 mb-8 min-w-[350px]" >
+      <div className="flex-1 mb-8 min-w-[300px]" >
         <h1 className="text-white text-2xl font-bold mb-5">Newsletter</h1>
         <div className="w-full">
           <p className="text-white">Stay updated with our latest trends</p>
@@ -25,7 +25,7 @@ const Footer = () => {
       </div>
       <div className="flex-1 mb-8">
         <h1 className="text-white text-2xl font-bold mb-5">Let us be social</h1>
-        <div className="flex min-w-[350px]">
+        <div className="flex min-w-[300px]">
           <NavLink to='https://www.facebook.com/juwon.asehinde.7' className="text-white w-12 h-12 flex items-center justify-center"><FaFacebookF style={{ width: "25px", height: "25px" }} /></NavLink>
           <NavLink to='https://github.com/Empire688682' className="text-white w-12 h-12 flex items-center justify-center"><BsGithub style={{ width: "25px", height: "25px" }} /></NavLink>
           <NavLink to='https://www.instagram.com/jahwonempire/' className="text-white w-12 h-12 flex items-center justify-center"><BsInstagram style={{ width: "25px", height: "25px" }} /></NavLink>
