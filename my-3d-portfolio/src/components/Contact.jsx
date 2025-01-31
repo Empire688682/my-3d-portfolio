ceb65b5df557cc6a285d1b9d1e7d4bb6c32f0968
Empire688682 <asehindej@gmail.com>
2025-01-31 18:28:29 +0100

    disabled={loading}

--- my-3d-portfolio/src/components/Contact.jsx
+++ my-3d-portfolio/src/components/Contact.jsx
@@ -121,6 +121,7 @@ const Contact = () => {
           }
 
           <button
+            disabled={loading}
             type='submit'
             className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'
           >
